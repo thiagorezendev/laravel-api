8e071b3c0b58a1b22e44449ef818671bdca91387
thiagorezendev <thiagor3zende@gmail.com>
2025-02-28 10:16:11 -0300

    initializing store invoices

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\InvoiceResource;
 use App\Models\Invoice;
 use Illuminate\Http\Request;
+use Validator;
 
 class InvoiceController extends Controller
 {
@@ -22,7 +23,7 @@ class InvoiceController extends Controller
      */
     public function create()
     {
-        //
+        //NÃO USAR POIS NÃO TEREMOS VIEW (É UMA API)
     }
 
     /**
@@ -30,7 +31,17 @@ class InvoiceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'user_id' => 'required',
+            'type' => 'required|max:1',
+            'paid' => 'required|numeric|between:0,1',
+            'payment_date' => 'nullable',
+            'value' => 'required|numeric|between:1,9999.99'
+        ]); 
+
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
     }
 
     /**
@@ -46,7 +57,7 @@ class InvoiceController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        //NÃO USAR POIS NÃO TEREMOS VIEW (É UMA API)
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ Route::prefix('v1')->group(function(){
     Route::get('/users/{user}', [UserController::class, 'show']);
     Route::get('/invoices', [InvoiceController::class, 'index']);
     Route::get('/invoices/{invoice}', [InvoiceController::class, 'show']);
+    Route::post('/invoices', [InvoiceController::class, 'store']);
 });
 
 // Route::get('/user', function (Request $request) {
