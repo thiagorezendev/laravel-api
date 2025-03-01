b791571f12ce50999e603d8f4ea9f30e26069152
thiagorezendev <thiagor3zende@gmail.com>
2025-03-01 15:35:30 -0300

    updated invoices initializing

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -33,25 +33,25 @@ class InvoiceController extends Controller
      */
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'user_id' => 'required',
             'type' => 'required|max:1',
             'paid' => 'required|numeric|between:0,1',
             'payment_date' => 'nullable',
             'value' => 'required|numeric|between:1,9999.99'
-        ]); 
+        ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return $this->error('Data Invalid', 422, $validator->errors());
         }
 
         $created = Invoice::create($validator->validated());
 
-        if($created){
-            return $this->response('Invoice created.',200, new InvoiceResource($created->load('user')));
+        if ($created) {
+            return $this->response('Invoice created.', 200, new InvoiceResource($created->load('user')));
         }
-        return $this->error('Invoice not created.',400);
-        
+        return $this->error('Invoice not created.', 400);
+
     }
 
     /**
@@ -73,9 +73,35 @@ class InvoiceController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Invoice $invoice)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'type' => 'required|max:1',
+            'paid' => 'required|numeric|between:0,1',
+            'value' => 'required|numeric',
+            'payment_date' => 'nullable|date_format:Y-m-d H:i:s',
+        ]);
+
+        if($validator->fails()) {
+            return $this->error('Invalid update', 422, $validator->errors());
+        }
+
+        $valited = $validator->validated();
+
+        $updated = $invoice->update([
+            'user_id' => $valited['user_id'],
+            'type' => $valited['type'],
+            'paid' => $valited['paid'],
+            'value' => $valited['value'],
+            'payment_date' => $valited['paid'] ? $valited['payment_date'] : NULL
+        ]);
+
+        if($updated) {
+            return $this->response('Invoice updated', 200, new InvoiceResource($invoice->load('user')));
+        }
+
+        return $this->error('Invoice not updated', 422);
     }
 
     /**
